8e770fe392566f427981ccaea2e2a647f33db97f
fatahrez <fertahrez@gmail.com>
2019-06-30 23:57:04 +0300

    in app.module.ts fix the shared module bug

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FooterComponent } from './layout/footer/footer.component';
-import { HeaderComponent } from './layout/header/header.component';
+// import { FooterComponent } from './layout/footer/footer.component';
+// import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent, HeaderComponent],
+  declarations: [
+    // FooterComponent, 
+    // HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
