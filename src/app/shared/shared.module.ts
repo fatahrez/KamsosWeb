3e763321486203837712bfe84eb1023cc8099802
fatahrez <fertahrez@gmail.com>
2019-06-30 22:10:59 +0300

    in shared/layout/ add header component which contains shared navigation bar

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 
 @NgModule({
-  declarations: [FooterComponent],
+  declarations: [FooterComponent, HeaderComponent],
   imports: [
     CommonModule
   ]
