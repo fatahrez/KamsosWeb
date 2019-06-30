5fe3239b2d22a57d7244f335cac406f895c4c3ba
fatahrez <fertahrez@gmail.com>
2019-06-30 23:44:00 +0300

    update app level app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module'
+import {
+  SharedModule,
+  FooterComponent,
+  HeaderComponent,
+} from './shared';
+
+const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: true });
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule, 
+    rootRouting,
   ],
   providers: [],
   bootstrap: [AppComponent]
