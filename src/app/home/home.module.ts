c846c976a289b1f8b19d03a4207ac344c89286d1
fatahrez <fertahrez@gmail.com>
2019-06-30 23:31:15 +0300

    in home/home.module.ts write module settings for home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared';
+
+const homeRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: '',
+    component: HomeComponent
+  }
+]);
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
-  ]
+    homeRouting,
+    SharedModule
+  ],
+  providers: []
 })
 export class HomeModule { }
