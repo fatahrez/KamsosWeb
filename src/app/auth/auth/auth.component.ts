3df300e5a7343cb2ba4160916c4a2e924dcc3a25
fatahrez <fertahrez@gmail.com>
2019-07-02 18:38:55 +0300

    write logic for login and sign up forms in auth.component.ts

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,16 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({    
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css']
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
 
+  authType: String = '';
+  title: String = '';
+  isSubmitting: boolean = false;
   authForm: FormGroup;
 
   constructor(
+    private route: ActivatedRoute,
     private fb: FormBuilder
   ) {
     this.authForm = this.fb.group({
@@ -19,7 +24,18 @@ export class AuthComponent {
     });
    }
 
+   ngOnInit(){
+     this.route.url.subscribe(data => {
+       this.authType = data[data.length - 1].path;
+       this.title = (this.authType === 'login') ? 'Sign in': 'Sign Up';
+       if (this.authType === 'register') {
+         this.authForm.addControl('username', new FormControl('', Validators.required));
+       }
+     });
+   }
+
   submitForm() {
+    this.isSubmitting = true;
     let credentials = this.authForm.value;
     console.log(credentials);
     
