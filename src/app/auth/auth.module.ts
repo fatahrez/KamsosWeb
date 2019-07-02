85f3129e24161e3774fab8422eb3d62f484c2941
fatahrez <fertahrez@gmail.com>
2019-07-02 18:46:07 +0300

    in auth.module.ts set child navigation and AuthComponent for the login and register paths

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { AuthComponent } from './auth/auth.component';
+import { SharedModule } from '../shared';
+
+const authRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: 'login',
+    component: AuthComponent
+  },{
+    path: 'register',
+    component: AuthComponent
+  }
+]);
 
 @NgModule({
   declarations: [AuthComponent],
   imports: [
-    CommonModule
+    authRouting,
+    SharedModule
   ]
 })
 export class AuthModule { }
