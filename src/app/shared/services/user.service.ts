4bc5cf27a43d2f272eae01246f927868adf56601
fatahrez <fertahrez@gmail.com>
2019-07-02 22:01:31 +0300

    in user service add variables currentUserSubject, currentUser, isAuthentictedSubject and isAuthenticated. in constructor add apiService instance and HttpClient instance

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+  private currentUserSubject = new BehaviorSubject<User>(new User());
+  public currentUser = this.currentUserSubject.asObservable().distinctUntilChanged();
 
-  constructor() { }
+  private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+  public isAuthenticated = this.isAuthenticatedSubject.asObservable();
+
+  constructor(
+    private apiService: ApiService,
+    private http: HttpClient
+  ) { }
+
+  setAuth
 }
