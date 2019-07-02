95b0da4b510627ed7a6011d23e224b93e0de5b09
fatahrez <fertahrez@gmail.com>
2019-07-02 20:15:06 +0300

    in shared/services/api.service add function setHeaders() which sets HttpHeader for Content-Type and Accept variables as application/json

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  private setHeader(): HttpHeaders{
+    let headersConfig = {
+      'Content-Type': 'app;ication/json',
+      'Accept': 'application/json'
+    };
+
+    return new HttpHeaders(headersConfig);
+  }
 }
