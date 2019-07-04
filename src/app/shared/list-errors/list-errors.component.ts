3879ae6898ac4ea5477a86d0b04815f3777066a0
fatahrez <fertahrez@gmail.com>
2019-07-04 21:12:40 +0300

    in error-list.component.ts add logic for generating error list

--- src/app/shared/list-errors/list-errors.component.ts
+++ src/app/shared/list-errors/list-errors.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import { Errors } from '../models';
 
 @Component({
   selector: 'app-list-errors',
   templateUrl: './list-errors.component.html',
   styleUrls: ['./list-errors.component.css']
 })
-export class ListErrorsComponent implements OnInit {
+export class ListErrorsComponent {
+  formattedErrors: Array<string> = [];
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  @Input()
+  set errors(errorList: Errors) {
+    this.formattedErrors = [];
+
+    if(errorList.errors){
+      for(let field in errorList.errors){
+        this.formattedErrors.push(`${field} ${errorList.errors[field]}`);
+      }
+    }
+  };
+
+  get errorList() { return this.formattedErrors;}
 
 }
