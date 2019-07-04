908d0e9e0fc8f6882f66ecf0411763d9df611883
fatahrez <fertahrez@gmail.com>
2019-07-04 22:28:57 +0300

    finish writing api auth for login

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -29,7 +29,7 @@ export class UserService {
 
   attemptAuth(type, credentials): Observable<User>{
     let route = (type === 'login') ? '/login' : '';
-    return this.apiService.post("/users" + route, {user: credentials})
+    return this.apiService.post("/auth" + route, {user: credentials})
     .pipe(
       tap(data => {
         this.setAuth(data.user);
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api_url: 'https://127.0.0.1:8000/api/v1'
+  api_url: 'http://127.0.0.1:8000/api/v1'
 };
 
 /*
