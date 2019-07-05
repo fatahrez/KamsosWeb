ebfb4cc35d267e2dece8d629f9a5b36a2dd27136
fatahrez <fertahrez@gmail.com>
2019-07-05 23:53:17 +0300

    push code before editing

--- src/app/shared/services/api.service.ts
+++ src/app/shared/services/api.service.ts
@@ -14,6 +14,14 @@ export class ApiService {
     private http: HttpClient,
     private jwtService: JwtService
   ) { }
+  
+  private setHeaders(): HttpHeaders {
+    let headersConfig = {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json'
+    };
+    return new HttpHeaders(headersConfig);
+  }
 
   private formatError(error: any){
     return throwError(error.json())
@@ -25,7 +33,7 @@ export class ApiService {
   }
 
   post(path: string, body: Object = {}):  Observable<any> {
-    return this.http.post(`${environment.api_url}${path}`, JSON.stringify(body)).pipe(
+    return this.http.post(`${environment.api_url}${path}`, JSON.stringify(body), { headers: this.setHeaders() }).pipe(
         catchError(this.formatError)
     );  
   }
