444a257e4f3b8793d90f9f5de281d3f64827d0e6
fatahrez <fertahrez@gmail.com>
2019-07-10 15:57:22 +0300

    update project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 // import { HttpClientModule } from '@angular/common/http';
 // import { CommonModule } from '@angular/common';
 
@@ -8,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './home/home.module';
 import { AuthModule } from './auth/auth.module';
+import { MaterialModule } from './material';
 
 import {
   ApiService,
@@ -32,8 +34,10 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: tru
     BrowserModule,
     AppRoutingModule,
     AuthModule,
+    BrowserAnimationsModule,
     SharedModule,
     HomeModule,
+    MaterialModule,
     rootRouting,
   ],
   providers: [
