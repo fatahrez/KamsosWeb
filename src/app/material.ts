e4bd0dee04b5be0073f48ad24cd02dd2eec12ddd
Wess58 <wesleymbate@gmail.com>
2019-07-13 17:49:29 +0300

    injected necessary Material components

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,13 +1,69 @@
 import { NgModule } from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { CdkTreeModule } from '@angular/cdk/tree';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 
 @NgModule({
   imports: [
     MatButtonModule,
-    // MatCheckboxModule
+    MatFormFieldModule,
+    LayoutModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatTreeModule,
+    MatIconModule,
+    CdkTreeModule,
+    MatTabsModule,
+    MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule
+
   ],
   exports:[
     MatButtonModule,
+    MatFormFieldModule,
+    LayoutModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatMenuModule,
+    MatTooltipModule,
+    MatExpansionModule,
+    MatTreeModule,
+    MatIconModule,
+    CdkTreeModule,
+    MatTabsModule,
+    MatCardModule,
+    MatTableModule,
+    MatProgressSpinnerModule
+
     // MatCheckboxModule,
   ],
 })
