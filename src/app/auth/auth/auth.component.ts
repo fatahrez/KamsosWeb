4fcbc26ca68aa8fbbbeffbcd812ef263d4c4048b
Wess58 <wesleymbate@gmail.com>
2019-07-13 22:17:24 +0300

    changed to material input type

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Errors, UserService} from '../../core';
+import {MaterialModule} from '../../material';
 
 @Component({
   selector: 'app-auth',
