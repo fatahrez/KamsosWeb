26073a283eac4314e567c344021fd30e1d773960
fatahrez <fertahrez@gmail.com>
2019-07-18 16:11:25 +0300

    in auth/no-auth-guard write method canActivate() with Observable to implement no-auth

--- src/app/auth/no-auth-guard.service.ts
+++ src/app/auth/no-auth-guard.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { UserService } from '../core';
+import {map, take} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class NoAuthGuardService {
+export class NoAuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean>{
+    return this.userService.isAuthenticated.pipe(take(1), map(isAuth => !isAuth));
+  }
 }
