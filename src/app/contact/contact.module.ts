58d667e3cd83276a7ab5cbb0b15bc6659010d2fd
fatahrez <fertahrez@gmail.com>
2019-07-24 11:53:34 +0300

    write AboutModule for routing

---
+++ src/app/contact/contact.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { ContactComponent } from './contact/contact.component';
+import { SharedModule } from '../shared';
+import { MaterialModule } from '../material';
+
+const contactRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: contact,
+    component: ContactComponent
+  }
+]);
+
+@NgModule({
+  declarations: [ContactComponent],
+  imports: [
+    contactRouting,
+    SharedModule,
+    MaterialModule
+  ]
+})
+export class ContactModule { }
