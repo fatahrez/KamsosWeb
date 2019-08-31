4e98edc9ae6ff84e01fb997c5cd24749431a5eac
fatahrez <fertahrez@gmail.com>
2019-08-31 06:49:28 +0300

    write vet srvices in services/vets.service.ts to handle Request for vets

---
+++ src/app/core/services/vets.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from './api.service';
+import {Observable} from 'rxjs';
+import {Vet} from '../models';
+import {map, tap} from 'rxjs/operators';
+import {log} from 'util';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class VetsService {
+
+  constructor(
+    private apiService: ApiService
+  ) { }
+
+  getAll(): Observable<Vet[]> {
+    return this.apiService.get(`/vets/`)
+      .pipe(tap(_ => log(`fetched vets`)), map(data => {
+        return data;
+      }));
+  }
+}
