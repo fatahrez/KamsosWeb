11f25e47c78d03bd371b05f7a38f1dd8f20e820c
fatahrez <fertahrez@gmail.com>
2019-08-31 06:54:09 +0300

    add Model models/Vet.models.ts as interface to map to API data

---
+++ src/app/core/models/vet.model.ts
@@ -0,0 +1,12 @@
+export interface Vet {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+  username: string;
+  email: string;
+  telephoneNumber: string;
+  county: string;
+  subCounty: string;
+  qualification: string;
+  vetImage: string;
+}
