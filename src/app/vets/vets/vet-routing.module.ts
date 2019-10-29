12fb05a94198943463ce5eb5e34a547547e68dd7
Wess58 <wesleymbate@gmail.com>
2019-10-29 14:15:39 +0300

    fixed routing into vet , issues to be fixed

--- src/app/vets/vets/vet-routing.module.ts
+++ src/app/vets/vets/vet-routing.module.ts
@@ -1,20 +1,21 @@
-import {RouterModule, Routes} from '@angular/router';
-import {VetsComponent} from './vets.component';
-import {NgModule} from '@angular/core';
-import {VetResolverService} from './vet-resolver.service';
+import { RouterModule, Routes } from '@angular/router';
+import { VetsComponent } from './vets.component';
+import { NgModule } from '@angular/core';
+import { VetResolverService } from './vet-resolver.service';
 
 const routes: Routes = [
   {
     path: ':slug',
     component: VetsComponent,
-    resolve: {
-      vet: VetResolverService
-    }
+    // resolve: {
+    //   vet: VetResolverService
+    // }
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [VetResolverService]
 })
-export class VetRoutingModule {}
+export class VetRoutingModule { }
