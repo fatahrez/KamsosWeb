f7e4b3b0a5dce5dc319370c2f1c0463701cfec84
fatahrez <fertahrez@gmail.com>
2019-12-02 15:05:26 +0300

    add coming soon for functionality agrovet

--- src/app/agrovet/agrovet.module.ts
+++ src/app/agrovet/agrovet.module.ts
@@ -1,12 +1,21 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AgrovetComponent } from './agrovet/agrovet.component';
+import {RouterModule} from '@angular/router';
+import {ContactComponent} from '../contact/contact/contact.component';
 
-
+const agrovetRouting: ModuleWithProviders = RouterModule.forChild([
+  {
+    path: 'agrovet',
+    component: AgrovetComponent
+  }
+]);
 
 @NgModule({
-  declarations: [],
+  declarations: [AgrovetComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    agrovetRouting,
   ]
 })
 export class AgrovetModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import {
   FooterComponent,
   HeaderComponent,
 } from './shared';
+import {AgrovetModule} from './agrovet/agrovet.module';
 
 
 // const rootRouting: ModuleWithProviders =RouterModule.forRoot([], { useHash: false });
@@ -39,6 +40,7 @@ import {
     HomeModule,
     AboutModule,
     ContactModule,
+    AgrovetModule,
     ResourcesModule,
     MaterialModule,
     FormsModule,
